89309a37e00f21cf0c72f1758fdc0a64b891b0cc
Zhou Chang <achang.zhou@gmail.com>
2013-01-09 14:58:19 +0800

    Udp server is also working now. Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- desktop/main.cpp
+++ desktop/main.cpp
@@ -6,13 +6,14 @@
 
 int main(int argc, char *argv[]) {
     talk_base::Thread *streaming_thread = new talk_base::Thread();
+    streaming_thread->Start();
     
     RtpSinker *sinker = new RtpSinker(streaming_thread);
     
     std::string localAddr = "udp://127.0.0.1:1979";
     std::string description = "video:h.264:640:480";
     sinker->StartSinking(localAddr, description, 0x19791010);
-
-    streaming_thread->Run(); 
+    
+    talk_base::Thread::Current()->Run();
     return 0;
 }
